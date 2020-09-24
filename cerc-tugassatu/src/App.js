f4ad5ef4a211fefb948aa26e0cc969c5697e0930
Syahrizal Fauzi <syahrizalfauzi16@gmail.com>
2020-09-25 06:59:16 +0700

    Initial commit

--- cerc-tugassatu/src/App.js
+++ cerc-tugassatu/src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/syahrizalfauzi/cerc-web-tugas1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Klik disini!
         </a>
       </header>
     </div>
